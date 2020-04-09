c3af9c5cefa0ef78a14f6c29e9e841f131de2c8f
nathanwang <wang.p@husky.neu.edu>
2020-04-08 20:17:13 -0400

    update UserController

--- src/main/java/edu/northeastern/cs5200/controllers/user/UserController.java
+++ src/main/java/edu/northeastern/cs5200/controllers/user/UserController.java
@@ -1,44 +1,43 @@
 package edu.northeastern.cs5200.controllers.user;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 @RestController
 public class UserController {
-	
+    private final UserRepository userRepository;
 
-	@Autowired
-	UserRepository userRepository;
-	
-	@RequestMapping("/api/user/description")
-	public UserObject sayUserObject() {
-		UserObject user = new UserObject("test","password");
-		return user;
-	}
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-	
+    @CrossOrigin
+    @GetMapping("/api/user/description")
+    public UserObject sayUserObject() {
+        UserObject user = new UserObject("user", "password");
+        return user;
+    }
 
 
+    @CrossOrigin
+    @GetMapping("/api/user/insert/{username}/{pass}")
+    public UserObject insertUser(@PathVariable("username") String user, @PathVariable("pass") String pass) {
+        UserObject userObj = new UserObject(user, pass);
+        userRepository.save(userObj);
+        return userObj;
+    }
 
-@RequestMapping("/api/user/insert/{username}/{pass}")
-public UserObject insertUser(@PathVariable("username") String user,@PathVariable("pass") String pass ) {
-	UserObject userObj = new UserObject(user,pass);
-	userRepository.save(userObj);
-	return userObj;
-}
-
-@RequestMapping("/api/users/select/all")
-public List<UserObject> selectAllUserObjects() {
-	List<UserObject> users =
-		(List<UserObject>)userRepository.findAll();
-	return users;
-}
-
+    @CrossOrigin
+    @GetMapping("/api/users/select/all")
+    public List<UserObject> selectAllUserObjects() {
+        List<UserObject> users =
+                (List<UserObject>) userRepository.findAll();
+        return users;
+    }
 
 
 }
