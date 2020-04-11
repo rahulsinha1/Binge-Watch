9ffbef8775dd0d516f241f10e6483261f707e26f
nathanwang <wang.p@husky.neu.edu>
2020-04-11 02:54:07 -0400

    added swagger stash

--- src/main/java/edu/northeastern/cs5200/Cs5200Spring2020Team2Application.java
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2020Team2Application.java
@@ -1,11 +1,15 @@
 package edu.northeastern.cs5200;
 
+import edu.northeastern.cs5200.controllers.user.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import edu.northeastern.cs5200.controllers.user.UserRepository;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class Cs5200Spring2020Team2Application {
