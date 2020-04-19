642a8ba269df357f40b2f5848694c41a13125e89
nathanwang <wang.p@husky.neu.edu>
2020-04-19 14:10:47 -0400

    add login credential, including: (#9)
    login, logout, currentuser and cookie, and login authorize in user Repository

--- src/main/java/edu/northeastern/cs5200/repository/UserRepository.java
+++ src/main/java/edu/northeastern/cs5200/repository/UserRepository.java
@@ -1,20 +1,21 @@
 package edu.northeastern.cs5200.repository;
 
-import java.util.List;
-
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
 import edu.northeastern.cs5200.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository
-	extends JpaRepository<User, Integer> {
+		extends JpaRepository<User, Integer> {
 
 	@Query("select u from User u where username = ?1")
 	public User findByUserName(String username);
 
+	@Query("SELECT user fROM User user WHERE user.username=:username and user.pass=:pass")
+	public User FindUserByCredentials(
+			@Param("username") String username,
+			@Param("pass") String pass
+	);
 	/*@Modifying
 	@Query("update User u set u.firstName = ?1, u.lastName = ?2 where u.id = ?1")
 	public User update(String username, User user);*/
