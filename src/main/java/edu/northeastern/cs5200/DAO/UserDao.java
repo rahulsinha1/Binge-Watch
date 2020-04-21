2245d5a360b0fb62ae3793cb3fd283d46fa6ef4c
nathanwang <wang.p@husky.neu.edu>
2020-04-20 22:09:01 -0400

    fixed minor issues. lost some class from last pull request

--- src/main/java/edu/northeastern/cs5200/DAO/UserDao.java
+++ src/main/java/edu/northeastern/cs5200/DAO/UserDao.java
@@ -1,9 +1,10 @@
 package edu.northeastern.cs5200.DAO;
 
 
+import edu.northeastern.cs5200.model.Movie;
 import edu.northeastern.cs5200.model.User;
 
-
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Optional;
 
