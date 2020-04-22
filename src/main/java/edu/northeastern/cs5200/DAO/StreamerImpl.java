871216c7ab0d664cb8d88b335cbfb369cc813937
nathanwang <wang.p@husky.neu.edu>
2020-04-22 00:43:43 -0400

    changed StreamerImpl

--- src/main/java/edu/northeastern/cs5200/DAO/StreamerImpl.java
+++ src/main/java/edu/northeastern/cs5200/DAO/StreamerImpl.java
@@ -1,15 +1,12 @@
 package edu.northeastern.cs5200.DAO;
 
-import edu.northeastern.cs5200.model.Movie;
 import edu.northeastern.cs5200.model.Streamer;
-import edu.northeastern.cs5200.repository.MovieRepository;
 import edu.northeastern.cs5200.repository.StreamerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 
 @RestController
@@ -28,15 +25,15 @@ public class StreamerImpl implements StreamerDao {
 
     @CrossOrigin
     @Override
-    @RequestMapping("api/streamers/delete")
-    public void deleteStreamer(int id) {
+    @RequestMapping("api/streamers/delete/{id}")
+    public void deleteStreamer(@PathVariable int id) {
         streamerRepository.deleteById(id);
     }
 
     @CrossOrigin
     @Override
-    @RequestMapping("api/streamers/find")
-    public List<Streamer> findStreamerByMovieName(String movieName) {
-        return (List<Streamer>)streamerRepository.findStreamerByMovie(movieName);
+    @RequestMapping("api/streamers/find/{movieName}")
+    public List<Streamer> findStreamerByMovieName(@PathVariable String movieName) {
+        return (List<Streamer>) streamerRepository.findStreamerByMovie(movieName);
     }
 }
--- src/main/java/edu/northeastern/cs5200/model/Critic.java
+++ src/main/java/edu/northeastern/cs5200/model/Critic.java
@@ -3,14 +3,11 @@ package edu.northeastern.cs5200.model;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 
@@ -18,12 +15,28 @@ public class Critic extends Person {
 
   private String company;
 
-  @OneToMany(mappedBy = "critic",fetch = FetchType.EAGER)
+  @OneToMany(mappedBy = "critic", fetch = FetchType.EAGER)
   @Fetch(FetchMode.SUBSELECT)
   private List<MovieReview> movieReviews;
 
   public Critic() {
     this.movieReviews = new ArrayList<>();
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public void setCompany(String company) {
+    this.company = company;
+  }
+
+  public List<MovieReview> getMovieReviews() {
+    return movieReviews;
+  }
+
+  public void setMovieReviews(List<MovieReview> movieReviews) {
+    this.movieReviews = movieReviews;
   }
 
   public Critic(String firstName, String lastName, String username, String pass, String email, List<Phone> phone, Address address, Role role, String company) {
