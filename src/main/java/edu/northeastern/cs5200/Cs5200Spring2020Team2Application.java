6edd30aa75f47804515c3d31352bb8e65aeb775e
Rahul Sinha <rahul.ksinha24@gmail.com>
2020-04-22 20:41:13 -0400

    aws

--- src/main/java/edu/northeastern/cs5200/Cs5200Spring2020Team2Application.java
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2020Team2Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -16,7 +19,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Cs5200Spring2020Team2Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Cs5200Spring2020Team2Application.class, args);
+    SpringApplication app = new SpringApplication(Cs5200Spring2020Team2Application.class);
+    app.setDefaultProperties(Collections
+            .singletonMap("server.port", "8083"));
+    app.run(args);
   }
 
 
